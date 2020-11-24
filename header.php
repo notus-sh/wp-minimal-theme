a0131f987c3772c06a698fe384652a5abeff8d2d
Gaël-Ian Havard <gaelian.havard@gmail.com>
2020-11-24 14:36:27 +0100

    Refactor stylesheets (except blocks)

--- header.php
+++ header.php
@@ -29,20 +29,22 @@
                 <p class="site-description"><?php echo $description; ?></p>
             <?php endif; ?>
         </div>
-    
-        <nav aria-label="<?php esc_attr_e('Main menu', 'mt'); ?>" role="navigation">
-            <ul class="primary-menu reset-list-style">
-                <?php if (has_nav_menu('primary')): ?>
-                    <?php wp_nav_menu([
-                        'container' => '',
-                        'items_wrap' => '%3$s',
-                        'theme_location' => 'primary',
-                    ]); ?>
-                <?php else: ?>
-                    <?php wp_list_pages(['title_li' => false]); ?>
-                <?php endif; ?>
-            </ul>
-        </nav>
-    
-        <?php get_search_form(['label' => __('Search for:', 'mt')]); ?>
+
+        <div class="header-controls">
+            <nav aria-label="<?php esc_attr_e('Main menu', 'mt'); ?>" role="navigation">
+                <ul class="primary-menu">
+                    <?php if (has_nav_menu('primary')): ?>
+                        <?php wp_nav_menu([
+                            'container' => '',
+                            'items_wrap' => '%3$s',
+                            'theme_location' => 'primary',
+                        ]); ?>
+                    <?php else: ?>
+                        <?php wp_list_pages(['title_li' => false]); ?>
+                    <?php endif; ?>
+                </ul>
+            </nav>
+        
+            <?php get_search_form(['label' => __('Search for:', 'mt')]); ?>
+        </div>
     </header>
