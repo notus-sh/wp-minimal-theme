c55f4f00fe7c4bc41177ff85fdb90cd8681dbd4b
Gaël-Ian Havard <gaelian.havard@gmail.com>
2020-11-25 15:23:40 +0100

    Refactor blocks styles

--- partials/singular-as-list-item.php
+++ partials/singular-as-list-item.php
@@ -4,31 +4,32 @@
  * Display a singular item (page, post or custom post type) in a list
  */
 
+$class = has_post_thumbnail() ? 'header-cover' : '';
+$style = has_post_thumbnail() ? sprintf("background-image: url(%s);", get_the_post_thumbnail_url()) : '';
+
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-    <header>
-        <?php the_title('<h2 class="entry-title heading-size-1"><a href="' . esc_url(get_permalink()) . '">', '</a></h2>'); ?>
-    
-        <?php the_time(get_option('date_format')); ?>
-        <?php get_template_part('partials/singular/categories'); ?>
-        <?php get_template_part('partials/singular/tags'); ?>
-        <?php get_template_part('partials/singular/comments-count'); ?>
+    <header
+        <?php echo !empty($class) ? sprintf('class="%s"', $class) : ''; ?>
+        <?php echo !empty($style) ? sprintf('style="%s"', $style) : ''; ?>
+        >
+        <div class="entry-header">
+            <?php the_title('<h2 class="entry-title heading-size-1"><a href="' . esc_url(get_permalink()) . '">', '</a></h2>'); ?>
+  
+            <div class="entry-meta">
+                <?php the_time(get_option('date_format')); ?>
+                <?php get_template_part('partials/singular/categories'); ?>
+                <?php get_template_part('partials/singular/tags'); ?>
+                <?php get_template_part('partials/singular/comments-count'); ?>
+            </div>
+        </div>
     </header>
     
-    <?php if (has_post_thumbnail()): ?>
-        <figure class="featured-media">
-            <div class="featured-media-inner">
-                <?php the_post_thumbnail(); ?>
-                <?php if ('' !== $caption = get_the_post_thumbnail_caption()): ?>
-                    <figcaption><?php echo wp_kses_post($caption); ?></figcaption>
-                <?php endif; ?>
-            </div>
-        </figure>
+    <?php if (has_excerpt()): ?>
+        <div class="entry-intro">
+            <?php the_excerpt(); ?>
+        </div>
     <?php endif; ?>
-    
-    <div class="entry-content">
-        <?php the_excerpt(); ?>
-    </div>
 </article>
 
